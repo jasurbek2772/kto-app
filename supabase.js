a2ec113b4eb2ac4999b2e54f8fbe1baf0341677d
Jasurbek <abdukarimovJasurbek27@mail.ru>
2026-05-19 14:10:15 +0700

    new supabase

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://btedlwsuwkhlugllylvm.supabase.co';
-const supabaseAnonKey = 'sb_publishable_DLP-I2nq1dgR0gPqueLUoQ_lkFtFDlu';
+const supabaseUrl = 'https://dbchoqluyhhxbdwcgypc.supabase.co';
+const supabaseAnonKey = 'sb_publishable_CiMF8i-5hFPyMp0Ec7DFdQ_IBtJZ5TD';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
